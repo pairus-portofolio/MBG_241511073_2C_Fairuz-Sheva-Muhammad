a1415b4dd915a2e3097f4b9329e428f0ed2a5c68
pairus-portofolio <shevafairuz10@gmail.com>
2025-10-01 15:45:32 +0700

    Edit kadaluarsa

---
+++ MBG_241511073_2C_Fairuz-Sheva-Muhammad/app/Models/BahanBakuModel.php
@@ -0,0 +1,27 @@
+<?php
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class BahanBakuModel extends Model
+{
+    protected $table      = 'bahan_baku';
+    protected $primaryKey = 'id';
+    protected $allowedFields = [
+        'nama','kategori','jumlah','satuan','tanggal_masuk','tanggal_kadaluwarsa','status','created_at'
+    ];
+
+    // ambil semua data + hitung status secara dinamis
+    public function getAllWithStatus()
+    {
+        return $this->select("*, 
+            CASE 
+              WHEN jumlah = 0 THEN 'habis'
+              WHEN CURRENT_DATE() >= tanggal_kadaluarsa THEN 'kadaluwarsa'
+              WHEN DATEDIFF(tanggal_kadaluarsa, CURRENT_DATE()) <= 3 THEN 'segera_kadaluarsa'
+              ELSE 'tersedia'
+            END AS status_calc")
+            ->orderBy('nama', 'ASC')
+            ->findAll();
+    }
+}
